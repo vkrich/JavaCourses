cf0cb0e24520af7a71a00b74ffb594df795a9b9a
vkrich <krichevskiy@reksoft.ru>
2021-12-24 17:12:12 +0300

    task 5 done  -Java-

--- Lessons/src/main/java/ru/java/courses/Lesson5_Strings.java
+++ Lessons/src/main/java/ru/java/courses/Lesson5_Strings.java
@@ -14,7 +14,24 @@ public class Lesson5_Strings {
      * @return полученную строку.
      */
     public static String task1(String[] strings) {
-        return null;
+        if(strings.length == 0){
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+
+        for(int i = strings.length-1; i >= 0; i--){
+            boolean flag = false;
+            for(int j = i - 1; j >= 0; j--){
+                if(strings[i].equals(strings[j])){
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag){
+                result.insert(0, strings[i]);
+            }
+        }
+        return result.toString();
     }
 
     /**
@@ -28,7 +45,16 @@ public class Lesson5_Strings {
      * @return  массив из полученных строк отсортированный по названию книг по алфавиту.
      */
     public static String[] task2(String[][] pairs) {
-        return null;
+        String[] result = new String[pairs.length];
+        String[] author;
+        String title;
+        for(int i = 0; i < pairs.length; i++){
+            author = pairs[i][1].split(" ");
+            title = pairs[i][0].replace("\"", "");
+            title = "\"" + title + "\" ";
+            result[i] = title + author[0].charAt(0) + ". " + author[1].charAt(0) + ". " + author[2];
+        }
+        return result;
     }
 
     /**
@@ -43,6 +69,18 @@ public class Lesson5_Strings {
      * @return подстроку между найденными символами
      */
     public static String task3(String str, char symb1, char symb2) {
-        return null;
+        int index1 = str.indexOf(symb1);
+        int index2 = str.indexOf(symb2);
+        if (index1 != -1 && index2 != -1){
+            if (index2 < index1){
+                int tmp = index2;
+                index2 = index1;
+                index1 = tmp;
+            }
+            return str.substring(index1+1, index2);
+        }else {
+            return "";
+        }
+
     }
 }
